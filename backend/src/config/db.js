fda8c4d91993827dd3d69d599b20008f322778dc
aayushiie <aayushi.singh067@gmail.com>
2026-06-15 19:35:13 +0530

    update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from mongoose
-import {ENV} from "./env.js"
-// npm i mongoose@8.19.3
+// import mongoose from mongoose
+// import {ENV} from "./env.js"
+// // npm i mongoose@8.19.3
 
-export const connectDB = async() =>{
-    try{
-       const conn = await mongoose.connect(ENV.DB_URL)
-       console.log(`Connected to MONGODB: ${conn.connection.host}`)
-    } catch (error){
-        console.error("MONGODB connection error")
-        process.exit(1) // exit code 1 means failure, 0 means success
-    }
-}
+// export const connectDB = async() =>{
+//     try{
+//        const conn = await mongoose.connect(ENV.DB_URL)
+//        console.log(`Connected to MONGODB: ${conn.connection.host}`)
+//     } catch (error){
+//         console.error("MONGODB connection error")
+//         process.exit(1) // exit code 1 means failure, 0 means success
+//     }
+// }

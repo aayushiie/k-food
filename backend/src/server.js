b20b1960830a287030820a5aa124d767481791c9
aayushiie <aayushi.singh067@gmail.com>
2026-06-16 04:03:19 +0530

    fix monorepo path for frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,9 +21,9 @@ app.get('/api/health', (req, res)=>{
 
 // in production
 if (ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
-    app.get("/{*any}", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
+    app.use(express.static(path.join(__dirname, "admin/dist")))
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "admin", "dist", "index.html"))
     })
 }
 

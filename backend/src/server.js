5d8327b72cc8b41166665a4a78f97b65c33141b5
aayushiie <aayushi.singh067@gmail.com>
2026-08-17 23:23:18 +0530

    review and product routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,8 @@ import cors from "cors"
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
 
 
 
@@ -26,6 +28,8 @@ app.use(clerkMiddleware()) // adds auth object under req => req.auth
 app.use("/api/admin", adminRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/reviews", reviewRoutes)
+app.use("/api/products", productRoutes)
 
 app.get('/api/health', (req, res)=>{
     res.status(200).json({message: "Success"})
